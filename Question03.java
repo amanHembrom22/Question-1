2e2523128b21e6f832c5b75131c08ca52e3ffe91
amanHembrom22 <125041289+amanHembrom22@users.noreply.github.com>
2023-02-10 22:06:19 +0530

    Add files via upload

---
+++ Question03.java
@@ -0,0 +1,19 @@
+package JavaQuiz;
+
+public class Question03 {
+
+	public static void main(String[] args) {
+		table();
+		
+	}
+	static void table() {
+		String[] name = {"Amy","Boyd","Cathy","John","Peter"};
+		
+		int[] value = {9,10,12,14,15};
+		System.out.println("name \tvalue");
+		for(int i=0;i<name.length;i++) {
+			System.out.println(name[i]+"\t"+value[i]);
+		}
+	}
+
+}
